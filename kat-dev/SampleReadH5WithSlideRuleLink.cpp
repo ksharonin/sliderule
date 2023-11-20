b20fe22a60d25a7c2c7fdc77537d6ee5d145798a
ksharonin <ksharonin@berkeley.edu>
2023-11-20 05:10:42 +0000

    broken compile

--- kat-dev/SampleReadH5WithSlideRuleLink.cpp
+++ kat-dev/SampleReadH5WithSlideRuleLink.cpp
@@ -17,19 +17,8 @@ int main() {
     H5Coro::context_t* context = nullptr; // simplify to no context
     bool meta_only = false; // t if metadata only
 
-    // args for func
-    // (const Asset* asset, const char* resource, 
-    // const char* datasetname, RecordObject::valType_t valtype,
-    // long col, long startrow, long numrows, context_t* context, 
-    // bool _meta_only, uint32_t parent_trace_id)
-
-    // Call the H5Coro::read function
     H5Future::info_t result = H5Coro::read(asset, url, datasetname, 
     valtype, col, startrow, numrows, context, meta_only);
-
-    // Handle the result as needed
-    // Example: Print some information from the result
-    // std::cout << "Info: DataType = " << result.dataType << ", DataSize = " << result.dataSize << std::endl;
 
     H5Coro::deinit();
 
