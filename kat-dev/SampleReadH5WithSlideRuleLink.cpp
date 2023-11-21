fbe70656ce0ce4ad3da820c44026b3f818fe6495
ksharonin <ksharonin@berkeley.edu>
2023-11-21 20:33:47 +0000

    asset factory access, finalizing inputs

--- kat-dev/SampleReadH5WithSlideRuleLink.cpp
+++ kat-dev/SampleReadH5WithSlideRuleLink.cpp
@@ -5,10 +5,23 @@
 
 int main() {
 
+    // H5Coro::init(1);
     H5Coro::init(1);
     
-    const Asset* asset = nullptr;
-    const char* url = "/home/ubuntu/Downloads/ATL03_20230816235231_08822014_006_01.h5"; // Replace with your H5 file URL
+    //const Asset* asset = nullptr;
+    std::vector<const char*> attr_in = {
+        "local",
+        "local",
+        "file",
+        "/home/ubuntu/Downloads/ATL03_20230816235231_08822014_006_01.h5",
+        "nil",
+        "nil",
+        "nil"
+    };
+
+    const Asset* asset = Asset::assetFactory(NULL, attr_in);
+    // the path to the file that is appended to the path parameter of the Asset object you created
+    const char* url = "/home/ubuntu/Downloads/ATL03_20230816235231_08822014_006_01.h5";
     const char* datasetname = "";
     RecordObject::valType_t valtype = RecordObject::valType_t::DYNAMIC;
     long col = 0; // single dim
@@ -20,6 +33,8 @@ int main() {
     H5Future::info_t result = H5Coro::read(asset, url, datasetname, 
     valtype, col, startrow, numrows, context, meta_only);
 
+    // H5Coro::deinit();
+    delete asset;
     H5Coro::deinit();
 
     return 0;
